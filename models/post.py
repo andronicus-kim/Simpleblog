17c3b09738538362ba5bfaa907a1cb9af1239168
andronicus-kim <androkim.kim7@gmail.com>
2018-08-13 00:32:28 +0300

    creates json out of post object

--- models/post.py
+++ models/post.py
@@ -1,9 +1,24 @@
 
 class Post(object):
 
-    def __init__(self,id,blog_id,title,content,author):
+    def __init__(self,id,blog_id,title,content,author,date):
         self.id = id
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
+        self.created_date = date
+
+    def save_to_mongo(self):
+        Database.insert('posts',self.json())
+
+    def json(self):
+        return {
+            'id': self.id,
+            'blog_id': self.blog_id,
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+            'created_date': self.created_date
+
+        }
