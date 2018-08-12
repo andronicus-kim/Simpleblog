28c9a08833942fe2b1ff778b05d53d0111c7c6ce
andronicus-kim <androkim.kim7@gmail.com>
2018-08-13 00:25:01 +0300

    more Post properties

--- models/post.py
+++ models/post.py
@@ -1,6 +1,9 @@
+
 class Post(object):
 
-    def __init__(self,title,content,author):
+    def __init__(self,id,blog_id,title,content,author):
+        self.id = id
+        self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
