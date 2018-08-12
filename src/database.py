3aaba0293d294b4c181fd3e66873aefb0560408d
andronicus-kim <androkim.kim7@gmail.com>
2018-08-13 00:53:00 +0300

    sets up and initializes database

--- src/database.py
+++ src/database.py
@@ -1,3 +1,13 @@
+import pymongo
+
 
 class Database(object):
-    pass
+    URI = "mongodb://127.0.0.1:27017"
+
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+
+        Database.DATABASE = client['fullstack']
